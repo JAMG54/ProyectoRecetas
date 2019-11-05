eb6fe491acfd0d18d67616bf8dd848c410a15d80
Marroquin2897 <57332287+Marroquin2897@users.noreply.github.com>
2019-11-05 09:40:30 -0600

    8

--- dataBase/BaseDatos.php
+++ dataBase/BaseDatos.php
@@ -4,7 +4,7 @@ class BaseDatos {
     
     public $servidor="localhost";
     public $usuario="root"; //usuario del mysql
-    public $clave="n0m3l0"; //contraña del mysql
+    public $clave=""; //contraña del mysql
     public $nombreBD="RecetarioFinal"; //nombre de la base de datos que vamos a conectar
 }
 ?>
